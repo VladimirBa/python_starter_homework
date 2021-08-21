b25ef513f7a2280a64f75d2ca86319e748a63c94
Vladimir <vldmr510@gmail.com>
2021-08-21 14:14:54 +0300

    007_Collection Data Types 1- homework has been added.

---
+++ 007_Collection Data Types 1/007_homework_2.py
@@ -0,0 +1,11 @@
+a = [0, - 125, 2, 4, 65, 0.111, 64, 65, 32, 2, 6, - 123, 0, -0.025, -1, 3]
+
+
+def list1():
+    print(max(a))
+    print(min(a))
+    print(f'{sum(a):.3f}')
+    print(f'{sum(a) / len(a):.3f}')
+
+
+list1()
