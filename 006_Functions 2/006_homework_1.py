1b644f0b031de7137173141314ab59f271fe9871
Vladimir <vldmr510@gmail.com>
2021-08-21 13:55:40 +0300

    006_Functions 2 - homework has been added.

--- 006_Functions 2/006_homework_1.py
+++ 006_Functions 2/006_homework_1.py
@@ -10,3 +10,30 @@ def greeting(z):
 
 
 greeting(z)
+
+
+def out_f():
+    x = 1
+
+    def inn_f():
+        global x
+        print(x)
+        x = 2
+        print(x)
+
+    inn_f()
+    print(x)
+
+    def in_inn_f():
+        nonlocal x
+        x = 3
+
+    print(x)
+    in_inn_f()
+    print(x)
+
+
+x = 10
+print(x)
+out_f()
+print(x)
