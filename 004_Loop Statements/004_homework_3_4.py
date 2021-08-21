ffc75e66ddd120e9077edaec70664729c7ac1c4d
Vladimir <vldmr510@gmail.com>
2021-08-21 13:12:00 +0300

    004_Loop Statements - homework 3-4 has been added.

--- 004_Loop Statements/004_homework_3_4.py
+++ 004_Loop Statements/004_homework_3_4.py
@@ -1,8 +1,13 @@
-height = int(input("Высота \"левого\" прямоугольного треугольника: "))
-for row in range(height):
-    for column in range(height):
-        if row + 1 > column:
-            print('*', end="")
+H = int(input('Высота "левого" прямоугольного треугольника: '))
+i = 0
+while i < H:
+    j = 0
+    while i + 1 > j:
+        if j <= H - 1:
+            print('*', end='')
+            j += 1
         else:
-            print(' ', end="")
+            print(' ', end='')
+            j += 1
+    i += 1
     print()
