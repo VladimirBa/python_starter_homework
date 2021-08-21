30a9a32621e8ca288d07f39b6f8c5253c3f22168
Vladimir <vldmr510@gmail.com>
2021-08-21 16:25:38 +0300

    008_Collection Data Types 2 - - practical has been added.

--- 008_collection_data_types_2/008_practical_3.py
+++ 008_collection_data_types_2/008_practical_3.py
@@ -10,3 +10,18 @@ for i in d_list:
         b_e_list.append(i)
 print("Максимальное числовое значение словаря d: ", max(a_e_list))
 print("Максимальное не числовое значение словаря d: ", max(b_e_list))
+
+#######
+
+d = {'a': 3, 'b': 'hello', 'c': 4, 'd': -3}
+d_list = list(d.values())
+a_e_list = []
+b_e_list = []
+
+for i in d_list:
+    if isinstance(i, int):
+        a_e_list.append(i)
+    else:
+        b_e_list.append(i)
+print("Максимальное числовое значение словаря d: ", max(a_e_list))
+print("Максимальное не числовое значение словаря d: ", max(b_e_list))
